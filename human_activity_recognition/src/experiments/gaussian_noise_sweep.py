ea675c0b0e3c8d343b71ef40e560041373717e7b
Richard Matthews <richard_matthews@apple.com>
2025-11-25 15:27:41 -0500

    Update gaussian noise experiment, it ran so probably works fine?

--- human_activity_recognition/src/experiments/gaussian_noise_sweep.py
+++ human_activity_recognition/src/experiments/gaussian_noise_sweep.py
@@ -5,30 +5,63 @@ from datetime import datetime
 import uuid
 
 from omegaconf import DictConfig
-from preprocess import load_and_filter_dataset_from_config, segment_dataset_from_config, train_test_split_pandas_df
+from preprocess import load_and_filter_dataset_from_config, segment_presplit_dataset_using_config
 from train import train
-from experiments.experiment_utils import start_child_run
+from experiments.experiment_utils import (kfold_train_val_test, start_child_run,
+                                          DatasetTriplet, SubjectListTriplet)
+
+def set_mlflow_tags(train_subjects, cv_subjects, test_subjects, excluded_subjects,
+                   noise_sweep_start, noise_sweep_end, noise_sweep_step, num_runs_per_step):
+    mlflow.set_tags({
+        "train_subjects": train_subjects,
+        "cv_subjects": cv_subjects,
+        "test_subjects": test_subjects,
+        "excluded_subjects": excluded_subjects,
+        "noise_sweep_start": noise_sweep_start,
+        "noise_sweep_end": noise_sweep_end,
+        "noise_sweep_step": noise_sweep_step,
+        "num_runs_per_step": num_runs_per_step,
+        "kfold": True
+    })
 
 def gaussian_noise_experiment(configs: DictConfig = None) -> None:
-    noise_sweep_start, noise_sweep_end, noise_sweep_step, num_runs_per_step = get_experiment_params(cfg=configs)
+    noise_sweep_start, noise_sweep_end, noise_sweep_step, num_runs_per_step = (
+        get_experiment_params(cfg=configs))
+
+    train_subjects, cv_subjects, test_subjects, excluded_subjects = (
+        get_experiment_cv_params(cfg=configs))
+
+    seed = configs.dataset.seed
+    input_shape = configs.training.model.input_shape
+
+    mlflow.log_params({"seed": configs.dataset.seed})
+    mlflow.log_params({"input_shape": input_shape})
+    mlflow.log_params({"input_length": input_shape[0]})
+
+
+    set_mlflow_tags(train_subjects=train_subjects,
+                    cv_subjects=cv_subjects,
+                    test_subjects=test_subjects,
+                    excluded_subjects=excluded_subjects,
+                    noise_sweep_start=noise_sweep_start,
+                    noise_sweep_end=noise_sweep_end,
+                    noise_sweep_step=noise_sweep_step,
+                    num_runs_per_step=num_runs_per_step)
 
     noise_values = np.arange(noise_sweep_start, noise_sweep_end, noise_sweep_step)
 
-    datasets = []
-    input_shapes = []
-
     dataset = load_and_filter_dataset_from_config(cfg=configs)
 
-    train_dataset, test_dataset = train_test_split_pandas_df(dataset=dataset,
-                                                             test_split=configs.dataset.test_split,
-                                                             seed=configs.dataset.seed)
+    (datasets, subjects_in_folds) = kfold_train_val_test(dataset=dataset,
+                                    subject_col="User",
+                                    test_subjects=test_subjects,
+                                    always_train_subjects=train_subjects,
+                                    cv_subjects=cv_subjects,
+                                    excluded_subjects=excluded_subjects,
+                                    n_splits=num_runs_per_step,
+                                    random_state=seed,
+                                    shuffle=True)
 
-    input_shape = configs.training.model.input_shape
-    print("[INFO] : Preprocessing for input shape: ", input_shape)
-    train_ds, valid_ds, test_ds = segment_dataset_from_config(cfg=configs, dataset=train_dataset, test_dataset=test_dataset)
-
-    mlflow.log_params({"input_shape": input_shape})
-    mlflow.log_params({"input_length": input_shape[0]})
 
     for noise_std in noise_values:
         with start_child_run(run_name=f"Noise_STD_{noise_std}",
@@ -37,14 +70,12 @@ def gaussian_noise_experiment(configs: DictConfig = None) -> None:
             child_run(noise_std=noise_std,
                       num_runs_per_step=num_runs_per_step,
                       configs=configs,
-                      train_ds=train_ds,
-                      valid_ds=valid_ds,
-                      test_ds=test_ds)
+                      datasets=datasets,
+                      subjects_in_folds=subjects_in_folds)
 
 def log_gaussian_noise_mlflow(cfg):
-    pass
-    # mlflow.log_param("gaussian_noise", cfg.preprocessing.gaussian_noise)
-    # mlflow.log_param("gaussian_std", cfg.preprocessing.gaussian_std)
+    mlflow.log_param("gaussian_noise", cfg.preprocessing.gaussian_noise)
+    mlflow.log_param("gaussian_std", cfg.preprocessing.gaussian_std)
 
 def get_experiment_params(cfg: DictConfig = None):
     if cfg.experiment.experiment_params is None:
@@ -69,12 +100,30 @@ def get_experiment_params(cfg: DictConfig = None):
 
     return noise_sweep_start, noise_sweep_end, noise_sweep_step, num_runs_per_step
 
+def get_experiment_cv_params(cfg: DictConfig):
+    params_config = cfg.dataset.train_val_test_cv_split
+
+    if params_config.train_subjects is None:
+        raise ValueError("train_subjects is None")
+    if params_config.cv_subjects is None:
+        raise ValueError("cv_subjects is None")
+    if params_config.test_subjects is None:
+        raise ValueError("test_subjects is None")
+    if params_config.excluded_subjects is None:
+        raise ValueError("excluded_subjects is None")
+
+    train_subjects = params_config.train_subjects
+    cv_subjects = params_config.cv_subjects
+    test_subjects = params_config.test_subjects
+    excluded_subjects = params_config.excluded_subjects
+
+    return train_subjects, cv_subjects, test_subjects, excluded_subjects
+
 def child_run(noise_std: float,
               num_runs_per_step: int,
               configs: DictConfig,
-              train_ds: tf.data.Dataset,
-              valid_ds: tf.data.Dataset,
-              test_ds: tf.data.Dataset) -> None:
+              datasets: list[DatasetTriplet],
+              subjects_in_folds: list[SubjectListTriplet]) -> None:
     if noise_std > 0:
         configs.preprocessing.gaussian_noise = True
         configs.preprocessing.gaussian_std = noise_std
@@ -85,8 +134,6 @@ def child_run(noise_std: float,
     print("[INFO] : Noise STD: ", configs.preprocessing.gaussian_std)
     log_gaussian_noise_mlflow(cfg=configs)
 
-    base_seed = configs.dataset.seed
-
     for i in range(num_runs_per_step):
         with start_child_run(run_name="Noise_STD_{}_Run_{}".format(noise_std, i),
                              inherit_params=True,
@@ -94,13 +141,23 @@ def child_run(noise_std: float,
             # Indicate that this run has no more children
             mlflow.set_tag("worker_run", "true")
 
-            seed = base_seed + 111 * i
-            configs.dataset.seed = seed
+            subjects_in_fold = subjects_in_folds[i]
 
-            print("[INFO] : Run number {} for noise std {}, seed {}".format(i, noise_std, configs.dataset.seed))
+            train_ds, valid_ds, test_ds, callbacks = (
+                segment_presplit_dataset_using_config(train_ds=datasets[i][0],
+                                                      val_ds=datasets[i][1],
+                                                      test_ds=datasets[i][2],
+                                                      cfg=configs))
 
-            mlflow.log_params({"seed": configs.dataset.seed})
+            print(f"[INFO] : Run number {i} for noise std {noise_std}")
+            print(f"[INFO] : Train subjects: {subjects_in_fold[0]}")
+            print(f"[INFO] : Validation subjects: {subjects_in_fold[1]}")
+            print(f"[INFO] : Test subjects: {subjects_in_fold[2]}")
 
-            train(cfg=configs, train_ds=train_ds, valid_ds=valid_ds, test_ds=test_ds, run_name=f"Noise_STD_{noise_std}_Run_{i}")
+            mlflow.log_params({"train_subjects": subjects_in_fold[0],
+                               "validation_subjects": subjects_in_fold[1],
+                               "test_subjects": subjects_in_fold[2]})
 
-    configs.dataset.seed = base_seed
+            train(cfg=configs, train_ds=train_ds, valid_ds=valid_ds,
+                  test_ds=test_ds, run_name=f"Noise_STD_{noise_std}_Run_{i}",
+                  callbacks=callbacks)
